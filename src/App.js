a47f02ca29e01db46784394d1d875c0747825953
OCTAVIO ARFA TENENBERG <47292048@ort>
2023-07-13 10:02:05 -0300

    cosas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
+  const [productosRandom, setProductosRandom] = useState([]); 
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+    .then(res => res.json())
+    .then((r) => {
+      console.log("1");
+      console.log(r);
+      console.log(r.products);
+      for (let i = 0; i < 6; i++) {
+        let pr;
+        do {
+          pr = r.products[Math.floor(Math.random()*29)]
+        } while(productosRandom.includes(pr))
+        setProductosRandom(productosRandom.toSpliced(0,0,pr))
+      }
+      console.log("2");
+    });
+  }, [])
+
+  useEffect(() => {
+    console.log(productosRandom);
+  }, [productosRandom])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='carousel slide'>
+        <div className='carousel-inner'>
+          {/* {productosRandom.map(pr => 
+            <div key={pr.id} className='carousel-item'>
+              <img src={pr.images[0]}></img>
+            </div>)} */}
+        </div>
+      </div>
     </div>
   );
 }
